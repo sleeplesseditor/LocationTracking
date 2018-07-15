8cee253441aed8ba97854268fe9ec95e8e6a9e1f
sleeplesseditor <josh@bryson.me.uk>
2018-07-15 17:51:23 +0100

    Haversine formula implementation

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View 
+} from 'react-native';
+import MapView from 'react-native-maps';
+import haversine from 'haversine';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -16,6 +22,54 @@ export default class App extends React.Component {
       })
     };
   }
+
+  componentDidMount() {
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        const { coordinate, routeCoordinates, distanceTravelled } =   this.state;
+        const { latitude, longitude } = position.coords;
+        
+        const newCoordinate = {
+          latitude,
+          longitude
+        };
+        //Component specific code for animating coordinate marker
+        if (Platform.OS === "android") {
+          if (this.marker) {
+            this.marker._component.animateMarkerToCoordinate(
+              newCoordinate,
+              500
+            );
+           }
+         } else {
+           coordinate.timing(newCoordinate).start();
+         }
+         this.setState({
+           latitude,
+           longitude,
+           routeCoordinates: routeCoordinates.concat([newCoordinate]),
+           distanceTravelled:
+           distanceTravelled + this.calcDistance(newCoordinate),
+           prevLatLng: newCoordinate
+         });
+       },
+       error => console.log(error),
+       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }
+
+  //Calculate distance travelled via Haversine formula
+  calcDistance = newLatLng => {
+    const { prevLatLng } = this.state;
+    return haversine(prevLatLng, newLatLng) || 0;
+  };
+
+  getMapRegion = () => ({
+    latitude: this.state.latitude,
+    longitude: this.state.longitude,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA
+  });
 
   render() {
     return (
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LocationTracking
 
-An experiment in real time location tracking with React Native and React Native Maps.
+An experiment in real time location tracking with React Native and React Native Maps. The app uses the Haversine formula to calculate the distance with longitude and latitude.
 
 This project was bootstrapped with [Create React Native App](https://github.com/react-community/create-react-native-app).
 
