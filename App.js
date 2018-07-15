4f17cc369c27a31eb09a6778b490412aa4d1906e
sleeplesseditor <josh@bryson.me.uk>
2018-07-15 17:32:02 +0100

    File structure refactoring

--- App.js
+++ App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: LATITUDE,
+      longitude: LONGITUDE,
+      routeCoordinates: [],
+      distanceTravelled: 0,
+      prevLatLng: {},
+      coordinate: new AnimatedRegion({
+       latitude: LATITUDE,
+       longitude: LONGITUDE
+      })
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
